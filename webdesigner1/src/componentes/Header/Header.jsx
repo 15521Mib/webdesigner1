15ddf05f4809be06285956b9bcd914fea3c3c98a
Logon Aluno <logonrmlocal@fiap.com.br>
2024-05-20 13:03:30 -0300

    webdesiner

--- webdesigner1/src/componentes/Header/Header.jsx
+++ webdesigner1/src/componentes/Header/Header.jsx
@@ -2,7 +2,7 @@ import Navbar from "../Navbar/Navbar"
 
 function Header() {
     return ( 
-        <header>
+        <header className="flex justify-around bg-primary-sus text-white p-4" >
             <span>Olá, traveller</span>
             <h1> Bem-vindo ao nosso site MovieApp!</h1>
             <Navbar/>
--- webdesigner1/src/pages/Home.jsx
+++ webdesigner1/src/pages/Home.jsx
@@ -1,7 +1,25 @@
+import data from '../../artigos.json'
+
 function Home() {
+    console.log(data)
     return ( 
-        <h1>Home</h1>
+        <main>
+            <inpunt type="text" name="busca" id="busca" placeholder="clique aqui para pesquisar o que busca"/>
+            <div className='lista-filmes'>
+                {
+                    data.map( 
+                        filme => (
+                            <h1 key={filme.title}>{filme.title}</h1>
+                        )
+                    )}
+            </div>
+        </main>
      );
 }
 
 export default Home;
+
+{/* <div className='card'>
+    <h1> Título do filme</h1>
+    </img src>
+</div> */}
