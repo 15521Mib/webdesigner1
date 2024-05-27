b2e3b48b8d749e229b4b638966683ceea9c0c07e
Logon Aluno <logonrmlocal@fiap.com.br>
2024-05-27 13:05:19 -0300

    aula

--- webdesigner1/src/pages/Home.jsx
+++ webdesigner1/src/pages/Home.jsx
@@ -4,15 +4,30 @@ function Home() {
     console.log(data)
     return ( 
         <main>
-            <inpunt type="text" name="busca" id="busca" placeholder="clique aqui para pesquisar o que busca"/>
-            <div className='lista-filmes'>
+            <input type="text" name="busca" id="busca" placeholder="clique aqui para pesquisar o que busca"/>
+            <div className='grid grid-cols-3'>
                 {
                     data.map( 
                         filme => (
-                            <h1 key={filme.title}>{filme.title}</h1>
+                            <div className='lista-filmes' key={filme.title}>
+                            <h1 >{filme.title}</h1>
+                            <img src={filme.image} alt={filme.title} />
+                            <div className='tag'>
+                                {filme.tags.map(tag => (
+                                    <span key={tag} className='text-purple-900'> {tag} </span>
+                                ))}
+                            </div>
+                            <div className="texto">
+                                {filme.text.map(texto => (
+
+                                    <p key={texto}> {texto}</p>
+                                ))}
+                            </div>
+
+                        </div>
                         )
                     )}
-            </div>
+             </div>
         </main>
      );
 }
