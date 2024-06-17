68580ff783614d4fb2e659cbc706fc41aa1e47ce
Logon Aluno <logonrmlocal@fiap.com.br>
2024-06-17 13:06:53 -0300

    update

--- webdesigner1/src/main.jsx
+++ webdesigner1/src/main.jsx
@@ -8,6 +8,7 @@ import Filmes from './pages/Filmes.jsx'
 import Sobre from './pages/Sobre.jsx'
 import Contato from './pages/Contato.jsx'
 import PageNotFound from './pages/PageNotFound.jsx'
+import PaginadeFilmes from './pages/PaginadeFilmes.jsx'
 
 
 const router = createBrowserRouter(
@@ -18,6 +19,7 @@ const router = createBrowserRouter(
        children: [
         {index: true, element: <Home/>},
         {path: "filmes", element: <Filmes/>},
+        {path: "filmes/:id", element: <PaginadeFilmes/>},
         {path: "sobre", element: <Sobre/>},
         {path: "contato", element: <Contato/>},
         {path: "*", element: <PageNotFound/>}
